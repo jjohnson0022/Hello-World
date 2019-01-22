73c7e86482e4b2e235056a33651f73398d3ad933
Jonathan <Jonathan@Blue>
2019-01-22 16:09:49 -0600

    Hello World test

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+package helloworld;
+
+/**
+ *Jonathan Johnson
+ * Hello World GitHub Project
+ * This will be my first project on GitHub.
+ */
+public class HelloWorld {
+
+    /**
+     *This will say hello to the world.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");        
+    }
+    
+}
